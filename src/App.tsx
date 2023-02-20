4ebd47a53d0937a3269a7865de41c379bb567bb5
Marcelo Both <marcelo.both@gmail.com>
2023-02-20 10:30:58 -0300

    feat: create new item and new card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import { FC } from "react"
-import { Column } from "./Column"
-import { Card } from "./Card"
-import { AppContainer } from "./styles"
+import { AddNewItem } from "./AddNewItem";
+import { AppContainer } from "./styles";
+import { Card } from "./Card";
+import { Column } from "./Column";
 
 export const App = () => {
   return (
     <AppContainer>
-      Columns will go here
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+      <AddNewItem
+        toggleButtonText="+ Add another list"
+        onAdd={console.log}
+      />
     </AppContainer>
   )
 }
--- src/Column.tsx
+++ src/Column.tsx
@@ -1,5 +1,6 @@
 import { ColumnContainer, ColumnTitle } from "./styles"
 import React, {FC} from "react";
+import {AddNewItem} from "./AddNewItem";
 
 type ColumnProps = {
   text: string
@@ -11,6 +12,12 @@ export const Column: FC<ColumnProps> = ({ text, children }: ColumnProps) => {
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
       {children}
+      
+      <AddNewItem
+        onAdd={console.log}
+        toggleButtonText="+ Add another card"
+        dark
+      />
     </ColumnContainer>
   )
 }
