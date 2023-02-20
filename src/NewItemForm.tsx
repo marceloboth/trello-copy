549c04211d813aa3d66232dbeecfef12d28c1899
Marcelo Both <marcelo.both@gmail.com>
2023-02-20 11:00:40 -0300

    feat: triggers form submission on pressing enter

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { NewItemButton, NewItemFormContainer, NewItemInput } from "./styles";
 import { useFocus } from "./utils/useFocus";
 
@@ -10,12 +10,22 @@ export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState("")
   const inputRef = useFocus()
 
+  const handleAddText = (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (event.key === 'Enter') {
+      onAdd(text)
+    }
+  }
+
   return (
     <NewItemFormContainer>
       <NewItemInput
         ref={inputRef}
         value={text}
-        onChange={e => setText(e.target.value)}/>
+        onChange={e => setText(e.target.value)}
+        onKeyPress={handleAddText}
+      />
       <NewItemButton onClick={() => onAdd(text)}>
         Create
       </NewItemButton>
