43569741734d3d9b1a5762582dda223b3d37b1f5
Marcelo Both <marcelo.both@gmail.com>
2023-02-22 16:54:57 -0300

    feat: hide column when is dragging

--- src/styles.ts
+++ src/styles.ts
@@ -17,7 +17,7 @@ interface DragPreviewContainerProps {
 
 export const DragPreviewContainer = styled.div<DragPreviewContainerProps>`
   transform: ${props => (props.isPreview ? "rotate(5deg)" : undefined)};
-  opacity: ${props => (props.isHidden ? 0.3 : 1)};
+  opacity: ${props => (props.isHidden ? 0 : 1)};
 `
 
 export const ColumnContainer = styled(DragPreviewContainer)`
--- src/utils/useItemDrag.ts
+++ src/utils/useItemDrag.ts
@@ -2,10 +2,12 @@ import { DragItem } from "../DragItem";
 import { useAppState } from "../state/AppStateContext";
 import { useDrag } from "react-dnd";
 import { setDraggedItem } from "../state/actions";
+import { useEffect } from "react";
+import { getEmptyImage } from "react-dnd-html5-backend";
 
 export const useItemDrag = (item: DragItem) => {
   const { dispatch } = useAppState()
-  const [, drag] = useDrag({
+  const [, drag, preview] = useDrag({
     type: item.type,
     item: () => {
       dispatch(setDraggedItem(item))
@@ -13,6 +15,10 @@ export const useItemDrag = (item: DragItem) => {
     },
     end: () => dispatch(setDraggedItem(null))
   })
+
+  useEffect(() => {
+    preview(getEmptyImage(), { captureDraggingState: true })
+  }, [preview])
 
   return { drag }
 }
